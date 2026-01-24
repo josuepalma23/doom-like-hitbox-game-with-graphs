694fd86393fd16495ffbbecb6d784571da28fe60
tjaden <195966262+josuepalma23@users.noreply.github.com>
2026-01-24 01:41:00 -0500

    cambios generales, la ventana ya vale con imagen y todo, le quiero aniadir una ventana previa con las reglas, y si se puede musica

--- EDA/vista.py
+++ EDA/vista.py
@@ -3,76 +3,62 @@ import random
 import math
 import os
 
-class VistaJuego:
+class Vista_Hitbox_Doom:
+    ## definimos atributos visuales
     def __init__(self, pantalla):
         self.pantalla = pantalla
-        self.temblor = 0 
+        self.temblor = 0
+        self.color_nodo = (200, 50, 50) ## color tipo sangre para los nodos
 
         carpeta_actual = os.path.dirname(os.path.abspath(__file__))
-        ruta_imagen = os.path.join(carpeta_actual, "monstruo.png")
-        
-        print(f"Buscando imagen en: {ruta_imagen}")
+        ruta_imagen = os.path.join(carpeta_actual, "disenio_eda.jpg")
 
-        if os.path.exists(ruta_imagen):
-            try:
-                imagen_original = pygame.image.load(ruta_imagen)
+        print(f"buscando img en {ruta_imagen}")
 
-                ancho_real, alto_real = imagen_original.get_size()
+        ##cargar el disenio
+        try:
+            self.disenio_entorno = pygame.image.load(ruta_imagen)
+            self.disenio_entorno = pygame.transform.scale(self.disenio_entorno, (800, 600))
+    
+        except:
+            print("No se encontro la imagen de fondo")
+            self.disenio_entorno = None
 
-                nuevo_alto = 500
-
-                factor = nuevo_alto / alto_real
-                nuevo_ancho = int(ancho_real * factor)
-
-                self.imagen_fondo = pygame.transform.scale(imagen_original, (nuevo_ancho, nuevo_alto))
-
-                x_centrado = (800 - nuevo_ancho) // 2
-                y_centrado = (600 - nuevo_alto) // 2
-                self.pos_imagen = (x_centrado, y_centrado)
-                
-                print(f"Imagen redimensionada a {nuevo_ancho}x{nuevo_alto} y centrada.")
-                
-            except Exception as e:
-                print(f"Error al cargar la imagen: {e}")
-                self.imagen_fondo = None
-        else:
-            print("ERROR: No encontré 'monstruo.png'. Revisa el nombre del archivo.")
-            self.imagen_fondo = None
-
-    def dibujar_nodo_transparente(self, x, y, radio):
-        superficie = pygame.Surface((radio*2, radio*2), pygame.SRCALPHA)
-        rojo_transparente = (255, 0, 0, 100) 
-        pygame.draw.circle(superficie, rojo_transparente, (radio, radio), radio)
-        self.pantalla.blit(superficie, (x - radio, y - radio))
-
+    '''
+    Dibujamos al juego por capas:
+        primero, el fondo
+        segundo, las aristas del grafo
+        tercero, los nodos
+    '''
     def dibujar_grafo(self, lista_nodos):
-        if self.imagen_fondo:
-            self.pantalla.blit(self.imagen_fondo, self.pos_imagen)
+
+        ## capa 1: el disenio o el fondo
+        if self.disenio_entorno:
+            self.pantalla.blit(self.disenio_entorno, (0,0)) ## dibujar el disenio en la pantalla
         else:
-            pygame.draw.rect(self.pantalla, (50, 50, 50), (200, 100, 400, 400))
+            self.pantalla.fill((20,20,20)) ##pintar todo gris si no hay disenio
 
+        ## capa 2: las aristas
         for nodo in lista_nodos:
-            for vecino in nodo.nodos_vecinos: 
-                pygame.draw.line(self.pantalla, (150, 150, 150), (nodo.x, nodo.y), (vecino.x, vecino.y), 3)
+            for vecino in nodo.vecinos:
+                ## dibujamos las aristas con metodos de pygame
+                pygame.draw.line(self.pantalla, (255, 0, 0), (nodo.x, nodo.y), (vecino.x, vecino.y), 5)
 
+        ## capa 3: los nodos
         for nodo in lista_nodos:
-            self.dibujar_nodo_transparente(nodo.x, nodo.y, nodo.radio)
-            pygame.draw.circle(self.pantalla, (255, 255, 255), (nodo.x, nodo.y), nodo.radio, 1)
+            ## dibujamos los circulos
+            pygame.draw.circle(self.pantalla, (255,255,255), (nodo.x, nodo.y), nodo.radio, 2)
 
 
     def dibujar_mira(self, mouse_x, mouse_y):
-        dx = random.randint(-self.temblor, self.temblor) if self.temblor > 0 else 0
-        dy = random.randint(-self.temblor, self.temblor) if self.temblor > 0 else 0
+        ## aumento de la sensibilidad del ads
+        dx = random.randint(-self.temblor, self.temblor)
+        dy = random.randint(-self.temblor, self.temblor)
         x_final, y_final = mouse_x + dx, mouse_y + dy
-        
-        pygame.draw.line(self.pantalla, (0, 255, 0), (x_final - 15, y_final), (x_final + 15, y_final), 2)
-        pygame.draw.line(self.pantalla, (0, 255, 0), (x_final, y_final - 15), (x_final, y_final + 15), 2)
+
+        color_mira = (0, 255, 0)
+        pygame.draw.line(self.pantalla, color_mira, (x_final - 20, y_final), (x_final + 20, y_final), 2)
+        pygame.draw.line(self.pantalla, color_mira, (x_final, y_final - 20), (x_final, y_final + 20), 2)
+        pygame.draw.circle(self.pantalla, color_mira, (x_final, y_final), 15, 1)
         
         return x_final, y_final
-
-    def detectar_impacto(self, x_mira, y_mira, lista_nodos):
-        for nodo in lista_nodos:
-            distancia = math.sqrt((x_mira - nodo.x)**2 + (y_mira - nodo.y)**2)
-            if distancia <= nodo.radio:
-                return nodo 
-        return None
